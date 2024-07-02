09f655e7a8bba3b9f9d180d34108197f21c81394
Abel Contreras <lucas_65_@hotmail.es>
2024-07-02 20:07:34 +0200

    Desplegando proyecto

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Template from '@template/Template.js';
-import "@styles/main.css"
-import "@styles/vars.styl"
-console.log('hola abel');
-
-(async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
-})();
